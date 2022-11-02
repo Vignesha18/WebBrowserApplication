1566720d4ebb927c0d8bb360bf2867b35f591c72
Vignesha18 <mvignesha3@gmail.com>
2022-11-02 15:50:35 +0530

    WebBrowserApplication

---
+++ app/src/main/java/com/example/shabrowser/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.shabrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    public WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=findViewById(R.id.website);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com/");
+    }
+}
